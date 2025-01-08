cbddd0bd112f779b6b5b165f0aea5444fc7de731
Emezie Ezem <emezieezem@gmail.com>
2025-01-08 13:33:14 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let hasSubmittedAssignment = true   //declaring variable that holds a boolean value
+let attendedPreviousClass = false
+
+
+// Function to check if access to a class is granted
+// the function accepts two parameters "hasSubmittedAssignment" and "attendedPreviousClass" 
+// If statement checks if both conditions are true
+
+function checkClassAccess(hasSubmittedAssignment, attendedPreviousClass) {
+  if (hasSubmittedAssignment && attendedPreviousClass) {
+    console.log("Access granted, you can attend")
+  }
+
+  else {
+    console.log("Access denied, submit your assignment and reach out to your class captain")
+  }
+
+}
+
+checkClassAccess(hasSubmittedAssignment, attendedPreviousClass);
